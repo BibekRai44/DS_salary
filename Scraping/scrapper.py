d8bf18025ea3d594547d955c916ea4aec226c763
Bibekkun <bibekrai655@gmail.com>
2023-04-27 09:18:53 +0545

    added timer

--- Scraping/scrapper.py
+++ Scraping/scrapper.py
@@ -1,8 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+driver = webdriver.Chrome('/Users/bibekrai/workspace/DS_salary/Scraping/chromedriver')
+driver.get('https://www.glassdoor.com/profile/login_input.htm')
 url="https://www.glassdoor.com/Job/data-science-jobs-SRCH_KO0,12.htm"
-response=requests.get(url)
-html_content=response.content
+username = driver.find_element_by_name('username') 
+password = driver.find_element_by_name('password')  
+from decouple import config
 
-soup=BeautifulSoup(html_content,'html.parser')
-print(response)
+username = config('username', default='')
+password = config('password', default='')
+
+
+username.send_keys('username')
+password.send_keys('password')  
+driver.find_element_by_class_name('gd-ui-button').click()
+time.sleep(3)
+driver.get(url)
